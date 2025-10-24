bd8fbe0ff3cc57128c7c3267576d463d555ef098
umairijaz57 <umairijaz2772@gmail.com>
2025-10-24 09:05:12 -0700

    steam tour

--- src/pages/services/stemtour.tsx
+++ src/pages/services/stemtour.tsx
@@ -11,17 +11,28 @@ export default function stemtour() {
   return (
     <main className="min-h-screen bg-blue-50">
       <Navbar />
- <section className="w-full h-[600px] relative flex items-center justify-center bg-white">
-                     
-                    {/* Background Image using Next.js Image component */}
-                    <Image
-                      src={steamtour}
-                      draggable={false}
-                      alt="STEAM Background"
-                      fill
-                      priority
-                    />
-                    </section>
+      <div className="max-w-full bg-[url(/jolly/bg.jpg)] bg-no-repeat bg-cover bg-center  w-full flex flex-col-reverse sm:flex-row gap-8 lg:gap-12">
+        <div className="">
+          <div className="relative flex flex-col w-full max-w-md">
+            <img
+              src="/masscot/bytebuddyR.png"
+              alt="Friendly robot character speaking"
+              className="w-full h-auto flip transform -scale-x-100"
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-center lg:justify-start items-start sm:mt-2 md:mt-6 lg:mt-12">
+          <div className=" rounded-full shadow-lg opacity-60 bg-blue-900 p-8 md:p-12 max-w-3xl border ">
+            <div className="text-center space-y-auto text-white">
+              <h1 className="text-3xl md:text-4xl font-bold  text-balance">STEAM TOUR: A day at NUST</h1>
+              <p className="text-base md:text-lg leading-relaxed capitalize">
+                Join us and discover the wonders of No.1 ranked university in Pakistan.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div className="flex flex-col md:flex-row justify-around py-20">
         <div className="bg-blue-50 font-bold border-l-blue-600 border-l-4 p-4 rounded-l-lg">
@@ -44,7 +55,7 @@ export default function stemtour() {
         </section>
       </div>
       {/* <ParentsTestimonials /> */}
-          <PieChart />
+      <PieChart />
       <Footer />
     </main>
   );
